ed1b23164a2da5ee4f99de56cfc9673cf6ae1f76
Neelesh kumar <neeleshkumarm98@gmail.com>
2023-12-12 07:07:18 +0530

    submit quiz and show attempt, marks and correct anshwer

--- src/app/pages/user/start/start.component.ts
+++ src/app/pages/user/start/start.component.ts
@@ -15,6 +15,15 @@ export class StartComponent implements OnInit{
   qid:any;
   question:any;
 
+
+  marksGot=0;
+  correctAnswer=0;
+  attempt=0;
+
+  isSubmit=false;
+
+
+
   constructor(private locationSt:LocationStrategy, private _route:ActivatedRoute,
     private _question:QuestionsService, private _Quiz:QuizService){}
 
@@ -39,9 +48,50 @@ export class StartComponent implements OnInit{
     this._question.getQuestionOfQuizForTest(this.qid).subscribe((data:any)=>{
       console.log(data);
       this.question=data;
+      
+      this.question.forEach((q:any) => {
+        q['givenAnswer']='';
+      });
+
     },(error)=>{
       console.log('error')
       Swal.fire("Error","Error in recieving the question form the server","error")
     })
+  }
+
+  submitQuiz(){
+    Swal.fire({
+      title: "Do you want to submit the Quiz ?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Submit",
+      icon:'info'
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        
+        this.isSubmit=true;
+        //calculate marks
+
+        this.question.forEach((q:any)=>{
+          if(q.givenAnswer==q.answer)
+          {
+            this.correctAnswer++;
+            let marksSingle=this.question[0].quiz.maxMarks/this.question.length
+            this.marksGot+=marksSingle;
+          }
+          if(q.givenAnswer.trim()!='')
+          {
+            this.attempt++;
+          }
+        })
+        // console.log("correct "+this.correctAnswer)
+        //   console.log("marks"+ this.marksGot)
+        //   console.log("attempt"+ this.attempt)
+
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
   }
 }
